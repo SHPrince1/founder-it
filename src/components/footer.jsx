42578cab844910e20599ecb3bdf00c7b8d2d2961
shprince1 <princehsylvanus@gmail.com>
2025-06-22 14:00:36 +0100

    icon changed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <>
       <div className={style.container}>
         <div>
-          <p>Copyright &copy; 2025, all right Reserved</p>
+          <p>Copyright &copy; <span style={{color: '#f7b084' }}>2025, All Right Reserved</span></p>
         </div>
         <div className={style.sMedia}>
           <div>

5becaee50f6eac3cd672fc2392154fa24830d58f
shprince1 <princehsylvanus@gmail.com>
2025-07-01 14:41:13 +0100

    home page imporeted as a component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 
-import BuildBusiness from './components/build-business';
-import Challenge from './components/challenge';
-import HompageBanner from './components/hompage-banner';
-import TopNav from './components/top-nav';
-import Footer from './components/footer'
+// import BuildBusiness from './components/build-business';
+// import Challenge from './components/challenge';
+// import HompageBanner from './components/hompage-banner';
+// import TopNav from './components/top-nav';
+// import Footer from './components/footer'
+import Home from './pages/home';
 
 
 function App() {
   return (
     <div>
-      <TopNav />
+
+      <Home />
+      {/* <TopNav />
       <HompageBanner />
       <BuildBusiness />
       <Challenge />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
